1095839d6dc6687d1b28412ed414d6f564c872d7
Andy Phillipson <ajp@mindspring.com>
2021-11-04 14:36:25 -0400

    Add missing import

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ export { VariantChat } from './components/VariantChat';
 export { useVariantChat } from './lib/VariantChat';
 export {
   initialVariantChatState,
+  variantChatReducer,
   VariantChatState,
 } from './store/slices/chat/chat';
